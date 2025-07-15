aade5d0dcfb360d17bfc41816437d80cdac74fee
prashanna2065 <prashanna2065@gmail.com>
2025-07-15 13:41:33 +0545

    adjustment

---
+++ script.js
@@ -0,0 +1,39 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+
+                try{
+        inputBox.value=eval(inputBox.value)
+    }
+    catch(error){
+        inputBox.value=math;
+        
+    }
+
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
+function calculate(){
+
+    
+}
+const car=[tesla, toyota, porshe, suzuki]
